c072e9b4e70f12fabab6b9cf531de3ed94546480
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-10 00:33:57 +0600

    tes

--- script.js
+++ script.js
@@ -2,38 +2,38 @@ let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 let numberInput = document.querySelector('#number-input');
 
-// numberInput.addEventListener('input', getFactAjax);
-// // Using ajax
-// function getFactAjax(){
-//     let number = numberInput.value;
+numberInput.addEventListener('input', getFactAjax);
+// Using ajax
+function getFactAjax(){
+    let number = numberInput.value;
     
-//     // Request to api
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', 'http://numbersapi.com/'+number);
-//     xhr.onload = function(){
-//         if(this.status == 200 && number !== ''){
-//             fact.style.display = 'block';
-//             factText.innerText = this.responseText;
-//         }
-//     }
+    // Request to api
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://numbersapi.com/'+number);
+    xhr.onload = function(){
+        if(this.status == 200 && number !== ''){
+            fact.style.display = 'block';
+            factText.innerText = this.responseText;
+        }
+    }
 
-//     xhr.send()
-// }
+    xhr.send()
+}
 
 // Using Fetch
-numberInput.addEventListener('input', getFactFetch);
+// numberInput.addEventListener('input', getFactFetch);
 
-function getFactFetch(){
-    let number = numberInput.value;
+// function getFactFetch(){
+//     let number = numberInput.value;
 
-    fetch('http://numbersapi.com/'+number)
-        .then(response => response.text())
-        .then((data)=>{
-            if(number != '') {
-                fact.style.display = 'block'
-                factText.innerText = data;
-            }
-        })
-        .catch(err => console.log(err))
+//     fetch('https://numbersapi.com/'+number)
+//         .then(response => response.text())
+//         .then((data)=>{
+//             if(number != '') {
+//                 fact.style.display = 'block'
+//                 factText.innerText = data;
+//             }
+//         })
+//         .catch(err => console.log(err))
 
-}
+// }
